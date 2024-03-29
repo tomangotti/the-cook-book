8a4b1e8593633128310374a92cd2faa47eb31237
thomas <tom.angotti11@gmail.com>
2024-03-29 09:52:48 -0600

    launch build

--- components/buttons/BackImageHeaderButton.js
+++ components/buttons/BackImageHeaderButton.js
@@ -3,6 +3,7 @@ import { TouchableOpacity, View, Image } from "react-native";
 
 const BackImageHeaderButton = ({handlePress}) => {
     const image = require("../../assets/images/back.png");
+    // const image = "https://cdn.icon-icons.com/icons2/953/PNG/512/back-button-left-arrow-symbol-in-a-square_icon-icons.com_74325.png"
     return (
         <TouchableOpacity onPress={handlePress}>
             <View style={{
